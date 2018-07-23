1026bf2385d9310343dffce8528695e3a62d291c
Karen Hoyos <karenuca@hotmail.com>
2018-07-23 15:46:20 +0200

    Get user input

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  onClick(event) {
+    event.preventDefault();
+    console.log("I am clicked")
+  }
+
+  onChange(event) {
+    let userInput = event.target.value
+      console.log(userInput)
+  }
+
   render() {
     return (
       <div>
         <form action="post">
-          <input placeholder="Select a cuisine..."></input>
-          <button type="submit">Submit</button>
+          <input onChange={this.onChange} placeholder="Select a cuisine..."></input>
+          <button onClick={this.onClick} type="submit">Submit</button>
         </form>
       </div>
     );
