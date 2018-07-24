1fe18879c26c01b12dc3b9730a25cedc84fd4753
Karen Hoyos <karenuca@hotmail.com>
2018-07-24 15:42:14 +0200

    Implement map and google api key

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Search from './Search';
+import MapContainer from './MapContainer';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
       <Search />
+      <MapContainer />
       </div>
     );
   }
