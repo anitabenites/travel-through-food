52a28491eddfb2bcd6a5862e02bbdd9a7c67eee4
Ana Benites <anitabenites2014@gmail.com>
2018-07-25 15:19:09 +0200

    Render Yelp API search results

--- src/YelpContainer.js
+++ src/YelpContainer.js
@@ -2,39 +2,50 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 const config = {
-  headers: {'Authorization': 'Bearer sX4fKJ-nINKvVQ_QyCLYGXMNE3AiZLK_Pj2_pLyxDWvhwNkY6MCYH_36ZH47ZkyQKKGAZYCzuoJ4moFo_P3-D3OL2CndzzHizE2d5FRMez87y2PUya1kwbnBiDBXW3Yx'},
+  headers: {'Authorization': "Bearer " + process.env.REACT_APP_YELP_API_KEY},
   params: {
     term: 'tacos',
-    location: 'london'
+    location: 'berlin'
   }
 };
 
 class YelpContainer extends Component {
 
-  componentWillMount() {
-    axios.get('https://shielded-hamlet-43668.herokuapp.com/'+'https://api.yelp.com/v3/businesses/search', config)
-    .then(function(response){
-      for (let i = 0; i < response.data.businesses.length; i++) {
-        let restaurant = {
-          name: response.data.businesses[i].name,
-          street: response.data.businesses[i].location.address1,
-          postcode: response.data.businesses[i].location.zip_code,
-          cuisine: response.data.businesses[i].categories[0].title,
-          rating:response.data.businesses[i].rating
-        }
-        console.log(restaurant);
-      }
-    })
-    }
-
-  render() {
-    return (
-      <div></div>
-    );
+  constructor(props) {
+  super(props);
+  this.state = {
+    restaurants: []
   }
 }
 
+  componentDidMount() {
 
+    axios.get('https://shielded-hamlet-43668.herokuapp.com/'+'https://api.yelp.com/v3/businesses/search', config)
+    .then(response => {
+      let restaurants = response.data.businesses.map((rest, index) => {
+        return(
+          <div key={index}>
+          <ul>
+            <p>Name: {rest.name}</p>
+            <p>Address:{rest.location.address1}</p>
+            <p>Postcode: {rest.location.zip_code}</p>
+            <p>Rating: {rest.rating}</p>
+            <p>--</p>
+          </ul>
+          </div>
+        );
+      });
+      this.setState({restaurants:restaurants});
+    });
+  };
 
+  render() {
+    return (
+      <div>
+      {this.state.restaurants}
+      </div>
+    );
+  };
+}
 
 export default YelpContainer;
